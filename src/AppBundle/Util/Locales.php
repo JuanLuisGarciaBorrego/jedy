9028026ef001e2e18874a25935274984c558da67
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-02 17:38:46 +0100

    service locales

---
+++ src/AppBundle/Util/Locales.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Util;
+
+use Symfony\Component\Intl\Intl;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class Locales
+{
+    /**
+     * @var string
+     */
+    private $locale_active;
+
+    /**
+     * @var array
+     */
+    private $locales;
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    public function __construct($locale_active, $locales, Session $session)
+    {
+        $this->locale_active = $locale_active;
+        $this->locales = $locales;
+        $this->session = $session;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLocales()
+    {
+        $localeCodes = explode('|', $this->locales);
+        $locales = array();
+
+        foreach ($localeCodes as $localeCode) {
+            $locales[] = [
+                'code' => $localeCode,
+                'name' => ucfirst(
+                    Intl::getLocaleBundle()->getLocaleName($localeCode, $this->session->get('locale_user'))
+                ),
+                'active' => ($this->locale_active == $localeCode ? true : false),
+            ];
+        }
+
+        return $locales;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocaleActive()
+    {
+        return $this->locale_active;
+    }
+}
---
+++ tests/AppBundle/Util/LocalesTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Util;
+
+use AppBundle\Util\Locales;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class LocalesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Locales
+     */
+    private $locales;
+
+    protected function setUp()
+    {
+        $session = new Session(new MockArraySessionStorage());
+        $session->set('locale_user', 'es');
+
+        $this->locales = new Locales("es", "es|en|fr", $session);
+    }
+
+    public function testGetLocales()
+    {
+        $result = $this->locales->getLocales();
+        $this->assertEquals($this->resultLocales(), $result);
+    }
+
+    public function testLocaleActive()
+    {
+        $result = $this->locales->getLocaleActive();
+        $this->assertEquals("es", $result);
+    }
+
+    public function resultLocales()
+    {
+        return [
+            [
+                'code' => "es",
+                'name' => "Español",
+                'active' => true,
+            ],
+            [
+                'code' => "en",
+                'name' => "Inglés",
+                'active' => false,
+            ],
+            [
+                'code' => "fr",
+                'name' => "Francés",
+                'active' => false,
+            ],
+        ];
+    }
+}
