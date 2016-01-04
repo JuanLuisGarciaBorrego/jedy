7ccc30fb00373bd9ef33024c6fa397719615b831
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-04 16:51:16 +0100

    name route post controller fixed

--- src/AppBundle/Controller/Admin/PostController.php
+++ src/AppBundle/Controller/Admin/PostController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class PostController extends Controller
 {
     /**
-     * @Route("s/", name="admin_home")
+     * @Route("s/", name="admin_post_home")
      */
     public function indexAction()
     {
