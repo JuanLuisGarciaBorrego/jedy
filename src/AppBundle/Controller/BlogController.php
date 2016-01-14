32569a755cddab850ac7c02b4fbe00926e8d3f82
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-14 13:15:36 +0100

    pagination in blog public posts

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -12,16 +12,26 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class BlogController extends Controller
 {
     /**
-     * @Route("/", name="app_blog_index")
+     * @Route("/", name="app_blog_index", defaults={"page" = 1})
+     * @Route("/page/{page}", name="app_blog_index_paginated", requirements={"page" : "\d+"})
      */
-    public function indexAction()
+    public function indexAction($page)
     {
+        $postPagination = $this->get('pagination')->pagination(
+            'post',
+            $page,
+            $this->get('request_stack')->getMasterRequest()->get('_locale'),
+            true
+        );
+
         return $this->render(
             'blog/blog_index.html.twig',
             [
-                'posts' => $this->getDoctrine()->getRepository('AppBundle:Content')->findBy(
-                    ['type' => 'post', 'locale' => $this->get('request_stack')->getMasterRequest()->get('_locale')]
-                ),
+                'posts' => $postPagination['contents'],
+                'type' => $postPagination['type'],
+                'total' => $postPagination['total'],
+                'totalPages' => $postPagination['totalPages'],
+                'page' => $page,
             ]
         );
     }
