7d1bf3680f85538fc76172613f7faaa1f1ab22c4
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-14 17:10:11 +0100

    homepage without posts

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -13,13 +13,6 @@ class AppController extends Controller
      */
     public function indexAction()
     {
-        return $this->render(
-            'app/app_index.html.twig',
-            [
-                'posts' => $this->getDoctrine()->getRepository('AppBundle:Content')->findBy(
-                    ['type' => 'post', 'locale' => $this->get('request_stack')->getMasterRequest()->get('_locale')]
-                ),
-            ]
-        );
+        return $this->render('app/app_index.html.twig');
     }
 }
