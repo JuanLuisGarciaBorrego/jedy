c1b8e38e67a9b989de80fc8aa623095b4c8d173b
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-25 16:05:06 +0100

    page fixtures fixed

--- src/AppBundle/DataFixtures/ORM/LoadPageData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPageData.php
@@ -26,7 +26,7 @@ class LoadPageData extends AbstractFixture implements OrderedFixtureInterface, C
         $locales = $this->container->get('locales');
 
         $page = new Content($locales->getLocaleActive());
-        $page->setTitle('What is a Jedy?');
+        $page->setTitle('What is a Jedi?');
         $page->setSummary('A Jedi was a Force-sensitive individual, most often a member of the Jedi Order, who studied, served, and used the mystical energies of the Force; usually, the light side of the Force');
         $page->setContent('The weapon of a Jedi was the lightsaber, a blade made of pure energy. Jedi fought for peace and justice in the Galactic Republic, usually against their mortal enemy: the Sith, who studied the dark side of the Force. The Jedi were all but destroyed by the Sith during and after the execution of Order 66, leaving very few Jedi survivors until there was only one known living Jedi, Luke Skywalker, at the end of the Galactic Civil War.');
         $page->setType('page');
