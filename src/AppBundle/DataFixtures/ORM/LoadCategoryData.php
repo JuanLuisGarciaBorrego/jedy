e6175e781b22204d0e6443021e46ac3246388667
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-01-25 10:44:05 +0100

    LoadCategory Data with translation

---
+++ src/AppBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\Category;
+
+class LoadCategoryData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $locales = $this->container->get('locales');
+
+        $category = new Category($locales->getLocaleActive());
+        $category->setName('Light side of the Force');
+        $category->setDescription(
+            'The Light Side of the Force was aligned with calmness and was used for knowledge and defense'
+        );
+
+        $manager->persist($category);
+
+        foreach ($locales->getLocales() as $locale) {
+
+            if (!$locale['active']) {
+                $translationCategory = new Category($locale['code']);
+                $translationCategory->setName($locale['code'].' Light side of the Force');
+                $translationCategory->setDescription(
+                    $locale['code'].' The Light Side of the Force was aligned with calmness and was used for knowledge and defense'
+                );
+                $translationCategory->setParentMultilangue($category);
+
+                $manager->persist($translationCategory);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
